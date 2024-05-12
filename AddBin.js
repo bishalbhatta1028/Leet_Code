e13956babe590ba6b270bcad35e49cf335ab0263
Bishal Bhatta <bishalbhatta1028@gmail.com>
2024-05-12 21:29:48 +0545

    add Binary number

---
+++ AddBin.js
@@ -0,0 +1,7 @@
+var addBinary = function (a, b) {
+  const aBin = `0b${a}`;
+  const bBin = `0b${b}`;
+  const sum = BigInt(aBin) + BigInt(bBin);
+  return sum.toString(2);
+};
+console.log(addBinary(11, 1));
